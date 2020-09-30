98ac2a5609cfe82d20f175ad9bc4f5b91e7e924e
Ryan Wendt <rwendt@bandwidth.com>
2020-09-30 00:19:26 -0400

    inital commit - time for some vm fun

---
+++ filter_plugins/disks.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+def get_disks(d):
+    i = 0
+    output = []
+    for k in d.keys():
+        disk_num = i
+        device = f"/dev/{k}"
+        size = d[k]["size"]
+        model = d[k]["model"]
+        output.append(f"Disk: {disk_num}")
+        output.append(f"Device: {device}")
+        output.append(f"Size: {size}")
+        output.append(f"Model: {model}")
+        output.append("")
+        i += 1
+    return output
+
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'disks': get_disks,
+        }
