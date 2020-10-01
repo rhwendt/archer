bebc4a24422cbf614bee9faa4a7064d1b7999ede
Ryan Wendt <rwendt@bandwidth.com>
2020-10-01 00:19:36 -0400

    update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/zsh
 
 # update repos
 pacman -Sy
@@ -9,9 +9,9 @@ pacman -S git ansible --noconfirm
 # clone repos
 git clone https://github.com/rhwendt/archer.git
 
-# sleep
-sleep 1
-
 # change to archer dir
 cd archer
+
+# run playbook
+ansible-playbook archer.yml
 
