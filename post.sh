3516ba2115490b7082fbd114919912b441187563
Ryan Wendt <rwendt@bandwidth.com>
2020-10-03 01:54:08 -0400

    hopefuly this works

--- post.sh
+++ post.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/zsh
 
-# run tzselect
-tzselect
+# set the timezone
+TIMEZONE=$(tzselect)
+echo "+ setting the timezone to $TIMEZONE"
+ln -sf /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime
+
+# install galaxy collection
+ansible-galaxy collection install community.general
+
+# run playbook
+ansible-playbook archer.yml --tags post
